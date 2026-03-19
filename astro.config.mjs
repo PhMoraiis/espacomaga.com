de23157db2aaed1d67d0c52ff289d5b5796b3f7c
Philipe Morais <liperapltda@gmail.com>
2026-03-19 00:25:18 -0300

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, fontProviders } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  fonts: [{
+    provider: fontProviders.google(),
+    name: 'Great Vibes',
+    cssVariable: '--font-great-vibes',
+  },
+  {
+      provider: fontProviders.google(),
+      name: 'Cormorant Garamond',
+      cssVariable: '--font-cormorant',
+  },
+  {
+      provider: fontProviders.google(),
+      name: 'Jost',
+      cssVariable: '--font-jost',
+  }],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+export const Hero = () => {
+	return (
+		<div>
+			<h1 className="text-4xl font-bold mb-4">Bem-vindo ao Espaço Maga!</h1>
+		</div>
+	);
+};
